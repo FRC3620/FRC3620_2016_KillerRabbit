68165d836ce81412d2e187128421350fc0c1bfa9
ToMoPAnae <ptfisch@gmail.com>
2016-02-01 18:35:23 -0500

    Began working on the shooter subsystem.

--- FRC3620_Killer_Rabbit/src/org/usfirst/frc3620/FRC3620_Killer_Rabbit/commands/ShootOutReverseCommand.java
+++ FRC3620_Killer_Rabbit/src/org/usfirst/frc3620/FRC3620_Killer_Rabbit/commands/ShootOutReverseCommand.java
@@ -42,7 +42,7 @@ public class ShootOutReverseCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.shooterSubsystem.ShootOutReverse();
+    	//Robot.shooterSubsystem.ShootOutReverse();
     }
 
     // Make this return true when this Command no longer needs to run execute()
