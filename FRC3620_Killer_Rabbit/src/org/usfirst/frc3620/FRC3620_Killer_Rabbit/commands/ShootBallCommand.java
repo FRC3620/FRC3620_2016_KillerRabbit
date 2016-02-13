88bc58f144a613a7fa81148becf5889f63d7dd24
slipshot711 <vameresh@gmail.com>
2016-02-12 19:16:48 -0500

    Fixed small logic error in Arm

--- FRC3620_Killer_Rabbit/src/org/usfirst/frc3620/FRC3620_Killer_Rabbit/commands/ShootBallCommand.java
+++ FRC3620_Killer_Rabbit/src/org/usfirst/frc3620/FRC3620_Killer_Rabbit/commands/ShootBallCommand.java
@@ -75,7 +75,7 @@ public class ShootBallCommand extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		if (canIShoot) {
+		if  (!canIShoot) {
 			return true;
 		}
 
