e6123e2ee6e0dded4995942689c48faec6fdf355
slipshot711 <vameresh@gmail.com>
2016-03-17 19:52:40 -0400

    Commiting to work on other laptop

---
+++ FRC3620_Killer_Rabbit/src/org/usfirst/frc3620/FRC3620_Killer_Rabbit/commands/DummyAutoCDF.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc3620.FRC3620_Killer_Rabbit.commands;
+
+import org.slf4j.Logger;
+import org.usfirst.frc3620.logger.EventLogging;
+import org.usfirst.frc3620.logger.EventLogging.Level;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DummyAutoCDF extends Command {
+
+	Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
+	Timer timer = new Timer ();
+	
+    public DummyAutoCDF() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	logger.info("doing CDF");
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return timer.hasPeriodPassed(1);
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
